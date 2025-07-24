dd690c9e130dd67cd8558686f9147d6c6222e62a
mobjihad <mobjihad15@gmail.com>
2025-07-25 00:29:05 +0600

    Empty Username & Password added

--- Lab 3 JS/script.js
+++ Lab 3 JS/script.js
@@ -2,7 +2,28 @@
 
 function check() {
 
-    let username = document.getElementById('username');
+    let username = document.getElementById('uname').value;
+    let password = document.getElementById('Password').value ; 
+
+    if(username==""){
+        let msg = document.getElementById('uname_msg');
+        msg.style.color = "red" ;
+        msg.innerHTML = "*Username Can't be Empty" ; 
+
+
+    }else{
+        document.getElementById('uname_msg').innerHTML = "";
+    }
     
+    if(password==""){
+        let msg = document.getElementById('pwd_msg');
+        msg.style.color = "red" ;
+        msg.innerHTML = "*Password Can't be Empty" ; 
+
+
+    }else{
+
+         document.getElementById('pwd_msg').innerHTML = "";
+    }
 
 }
