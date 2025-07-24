ffac2cf84fbb1dc7889b0dde56da1563ae65cddb
mobjihad <mobjihad15@gmail.com>
2025-07-25 00:47:06 +0600

    Password length check function added

--- Lab 3 JS/script.js
+++ Lab 3 JS/script.js
@@ -4,26 +4,33 @@ function check() {
 
     let username = document.getElementById('uname').value;
     let password = document.getElementById('Password').value ; 
-
+    let umsg = document.getElementById('uname_msg');
+    let pmsg = document.getElementById('pwd_msg');
     if(username==""){
-        let msg = document.getElementById('uname_msg');
-        msg.style.color = "red" ;
-        msg.innerHTML = "*Username Can't be Empty" ; 
+       
+        umsg.style.color = "red" ;
+        umsg.innerHTML = "*Username Can't be Empty" ; 
 
 
     }else{
-        document.getElementById('uname_msg').innerHTML = "";
+        umsg.innerHTML = "";
     }
     
     if(password==""){
-        let msg = document.getElementById('pwd_msg');
-        msg.style.color = "red" ;
-        msg.innerHTML = "*Password Can't be Empty" ; 
+        
+        pmsg.style.color = "red" ;
+        pmsg.innerHTML = "*Password Can't be Empty" ; 
 
 
+    }else if(password.length<8){
+        pmsg.style.color = "red" ; 
+        pmsg.innerHTML = "Password's Length can't be less than 8 digit"; 
+
+    
+        
     }else{
 
-         document.getElementById('pwd_msg').innerHTML = "";
+        pmsg.innerHTML = "";
     }
 
 }
