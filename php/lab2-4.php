aa9012b07cc3b02c8e1e7155ac039759dfdebc92
mobjihad <mobjihad15@gmail.com>
2025-08-20 16:29:54 +0600

    Login files

---
+++ php/lab2-4.php
@@ -0,0 +1,18 @@
+<?php
+
+$number1= 10 ; 
+$number2 = 20 ; 
+$number3 = 40 ; 
+
+if ($number1 > $number2 && $number1 > $number3){
+
+    echo "Number 1 {$number1} is the largest number " ; 
+}else if ($number2 > $number1 && $number2 > $number3 ){
+
+    echo "Number 2 {$number2} is the largest number " ; 
+}else {
+
+    "Number 3 {$number3} is the largest number " ; 
+}
+
+?> 
---
+++ php/login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>lOGIN</title>
+</head>
+<body>
+
+<form action="abc.php" method= "get">
+
+    Usernmae: 
+    <input type="text"></br>
+    Password: 
+    <input type="text"></br>
+    <input type="submit" value ="submit">
+</form>
+    
+</body>
+</html>
